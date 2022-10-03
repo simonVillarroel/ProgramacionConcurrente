fbacd6806d7fca91c9c6252dffd878291165c320
simonVillarroel <simon.villarroel@est.fi.uncoma.edu.ar>
2022-10-02 21:15:24 -0300

    mend

--- TP3/Punto4/Hamster.java
+++ TP3/Punto4/Hamster.java
@@ -9,26 +9,7 @@ public class Hamster implements Runnable{
         this.jaula = jaula;
     }
 
-    public void run(){
-
-        System.out.println();
-        comer();
-        ejercitar(jaula.getRueda().getTiempoDeEjercicio());
-        descansar(jaula.getHamaca().getTiempoDeDescanso());
-
-    }
-
-    public void comer(){
-        System.out.println(Thread.currentThread().getName() + " comiendo.");
-        jaula.getPlato().comer();
-        System.out.println(Thread.currentThread().getName() + " termino de comer.");
-    }
-
-    public void ejercitar(int tiempo){
-            System.out.println(Thread.currentThread().getName() + " ejercitando.");
-            jaula.getRueda().ejercitar();
-            System.out.println(Thread.currentThread().getName() + " termino de ejercitar.");
-
+    public void run(){            
         int actividad;
         Random random = new Random();
         //Cada Hamster intentara realizar las distintas actividades indefinidamente
